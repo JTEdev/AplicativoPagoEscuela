98709541a92b6d36c7ec53aceded099f37d27f05
Jose Antonio <35318647+JTEdev@users.noreply.github.com>
2025-06-18 22:58:07 -0500

    Correción redireccionamiento de Ver todos los pagos

--- pages/admin/AdminAllPaymentsPage.tsx
+++ pages/admin/AdminAllPaymentsPage.tsx
@@ -316,10 +316,10 @@ const handleEditPayment = async (e: React.FormEvent) => {
                 ))}
               </tbody>
             </table>
-            <div className="flex justify-end mt-4">
+            <div className="flex justify-end mt-4"> 
               <Button
                 variant="primary"
-                onClick={() => navigate('/admin/payments')}
+                onClick={() => navigate('/admin/dashboard')}
               >
                 {t('viewAllPayments')}
               </Button>
